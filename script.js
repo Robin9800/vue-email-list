281a9e8f3c67599a9cfe19d584bfd1d6153929c1
Robin9800 <robin.costanzo@gmail.com>
2022-04-04 17:06:01 +0200

    mounted content

---
+++ script.js
@@ -0,0 +1,21 @@
+// Attraverso l'apposita API di Boolean
+// https://flynn.boolean.careers/exercises/api/random/mail
+// generare 10 indirizzi email e stamparli in pagina
+// all'interno di una lista.
+const app = new Vue({
+    el: '#app',
+    data: {
+        email: '',
+        arrayEmail: []
+    },
+    mounted(){
+        //Ciclo che pusha dentro all'array un email generata a caso utilizzando l'axios di boolean per le email
+        for(let i=0; i< 10; i++){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response) =>{
+                const result = response.data
+                this.arrayEmail.push(this.email);
+                console.log(this.arrayEmail);
+            })
+        }
+    }
+})
