820c2197270eb988654fa3692a19c69b68c7f8c5
Robin9800 <robin.costanzo@gmail.com>
2022-04-04 17:11:11 +0200

    axios fixed

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ const app = new Vue({
         for(let i=0; i< 10; i++){
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response) =>{
                 const result = response.data
+                this.email = result.response
                 this.arrayEmail.push(this.email);
                 console.log(this.arrayEmail);
             })
